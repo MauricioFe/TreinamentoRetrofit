178b060f93b11b99be0d34294fc4f8ad08a16716
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-15 11:47:08 -0300

    Criando a classe de configuração do retrofit

--- app/src/main/java/com/example/treinamentoretrofit/MainActivity.java
+++ app/src/main/java/com/example/treinamentoretrofit/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.treinamentoretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSoap;
+    Button btnRest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSoap = findViewById(R.id.button);
+        btnRest = findViewById(R.id.button2);
+
+        btnSoap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SoapActivity.class));
+            }
+        });
+        btnRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SoapActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/treinamentoretrofit/RestActivity.java
+++ app/src/main/java/com/example/treinamentoretrofit/RestActivity.java
@@ -3,12 +3,28 @@ package com.example.treinamentoretrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.treinamentoretrofit.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestActivity extends AppCompatActivity {
+    List<Usuario> usuarioList;
+    ListView listaUsuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest);
+        listaUsuarios = findViewById(R.id.list_view_usuario);
+        usuarioList = new ArrayList<>();
+
+        atualizaListaUsuarios();
+    }
+
+    private void atualizaListaUsuarios() {
+
     }
 }
