966dd1c980425e358ae7a3f7389c25bd6a00ba32
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-15 10:43:24 -0300

    Criando os modelos de usuario e Produto

---
+++ app/src/main/java/com/example/treinamentoretrofit/models/Produto.java
@@ -0,0 +1,51 @@
+package com.example.treinamentoretrofit.models;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+    private int id;
+    private String Nome;
+    private double Preco;
+    private int Estoque;
+    private String Descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public double getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(double preco) {
+        Preco = preco;
+    }
+
+    public int getEstoque() {
+        return Estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        Estoque = estoque;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+}
